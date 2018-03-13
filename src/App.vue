ad10b5cf941af66a8bd59fa0a5ace42d88132a95
Nikita <Freenikita95@gmail.com>
2018-03-13 23:59:47 +0200

    revert commit 2

--- src/App.vue
+++ src/App.vue
@@ -21,55 +21,36 @@
 </template>
 
 <script>
-import HeaderSchedule from './components/HeaderSchedule'
-import LinkSchedule from './components/LinkSchedule'
-import MenuSchedule from './components/MenuSchedule'
-import MainSchedule from './components/MainSchedule'
+    import HeaderSchedule from './components/HeaderSchedule'
+    import LinkSchedule from './components/LinkSchedule'
+    import MenuSchedule from './components/MenuSchedule'
+    import MainSchedule from './components/MainSchedule'
+    export default {
+        name: 'app',
+        props:[
+            'dataOpen'
+        ],
+        components: {
+            HeaderSchedule,
+            LinkSchedule,
+            MenuSchedule,
+            MainSchedule
+        },
+        data () {
 
-export default {
-    name: 'app',
-    props:[
-        'dataOpen'
-    ],
-    components: {
-        HeaderSchedule,
-        LinkSchedule,
-        MenuSchedule,
-        MainSchedule
-    },
-    data () {
-        loadInfo: {
-            let xhr = new XMLHttpRequest();
-            xhr.open('GET', 'http://wb-predictivemaintenance-api.prsp7vkew2.eu-central-1.elasticbeanstalk.com/api/TorqueValues', false);
-            xhr.send();
-            if (xhr.status != 200) {
-                // обработать ошибку
-                alert('Ошибка ' + xhr.status + ': ' + xhr.statusText);
-            } else {
-                // let lenghtData = xhr.responseText.length;
-                let a = xhr.responseText
-                let b  = JSON.parse(a)
-
-                let dataOpen = b.map(function(name) {
-                    if (name.Direction == "Open") {
-                        return name
-                    }
-                });
-                console.log(dataOpen)
+            return {
+                menu: {
+                    title: 'TERMINAL OVERVIEW',
+                    imageLog: '../assets/logo.png',
+                    terminalName: 'WEST',
+                    countryCode:'NLD',
+                    address:'Some address'
+                },
+                loadData: ''
             }
         }
-        return {
-            menu: {
-                title: 'TERMINAL OVERVIEW',
-                imageLog: '',
-                terminalName: 'WEST',
-                countryCode:'NLD',
-                address:'Some address'
-            },
-            loadData: this.dataOpen
-        }
     }
-}
+
 </script>
 
 <style>
--- src/components/HeaderSchedule.vue
+++ src/components/HeaderSchedule.vue
@@ -18,5 +18,4 @@
         background: #fff;
         box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
     }
-
 </style>
